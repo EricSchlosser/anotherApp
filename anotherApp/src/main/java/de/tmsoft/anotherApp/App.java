73429fa53395a2d32ac71fc2de6827f28300cf83
Normalo <Normalo@DESKTOP-QCVHLOQ.fritz.box>
2019-11-02 09:08:35 +0100

    First commit

---
+++ anotherApp/src/main/java/de/tmsoft/anotherApp/App.java
@@ -0,0 +1,13 @@
+package de.tmsoft.anotherApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Another App" );
+    }
+}
